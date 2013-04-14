dc2939507fc3c03c37a42727fdb503c16c8935b7
Josep <apuratepp@gmail.com>
2013-04-14 21:27:54 +0200

    view per

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,8 +16,9 @@ class PagesController < ApplicationController
   # GET /pages/1
   # GET /pages/1.json
   def show
-    # @page = Page.find(params[:id])
-    @page = current_user.pages.find(params[:id])
+    #@page = Page.find(params[:id])
+    # @page = current_user.pages.find(params[:id])
+    @page = Page.where(:id => params[:id], :public => true).first
 
     respond_to do |format|
       format.html # show.html.erb
